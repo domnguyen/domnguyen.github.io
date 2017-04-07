571d47c2767df7e8d653984c5422088784ccc5fd
Dominique Nguyen <dom.nguyen1@gmail.com>
2017-04-07 10:20:08 -0700

    Fixed the Sticky Scroll

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(window).scroll(function() {
    var hT = $('.full-height').height(),
        wS = $(this).scrollTop();
     console.log((hT) , wS);
-   if (wS > (hT + 40)){
+   if (wS > (hT - 60)){
      $("nav.navbar").addClass("navbar-fixed-top");
    }
 	 else{
