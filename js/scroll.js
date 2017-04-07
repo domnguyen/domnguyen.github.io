3db3ceb1e351d6c803396b3a1626514c1cc03555
Dominique Nguyen <dom.nguyen1@gmail.com>
2017-04-07 10:37:18 -0700

    Added Links

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(window).scroll(function() {
    var hT = $('.full-height').height(),
        wS = $(this).scrollTop();
     console.log((hT) , wS);
-   if ((hT) > wS-60 ){
+   if ((hT) > (wS-60) ){
      $("nav.navbar").addClass("navbar-fixed-top");
    }
 	 else{
