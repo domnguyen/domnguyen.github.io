c41ff3defa69bc10ec7334644d26c9e129014c9f
Dominique Nguyen <dom.nguyen1@gmail.com>
2017-04-06 19:23:02 -0700

    Cleaned up Uncessary Files

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-	if($('.navbar').offset().top > 50){
+	if($('.navbar').offset().top > 100vh){
 		$('.navbar-fixed-top').addClass('top-nav-collapse');
 	} else {
 		$('.navbar-fixed-top').removeClass('top-nav-collapse');
